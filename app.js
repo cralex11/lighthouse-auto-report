fe02101e12ea830ca31d228ad14d343cb6c7a49d
cralex11 <cristeaalex7@gmail.com>
2021-05-23 20:41:34 +0300

    added all code

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 
 //config
-const GENERATE_NEW_REPORT = false;
+const GENERATE_NEW_REPORT = true;
 const OUTPUT_DIRECTORY_NAME = 'reports';
+//All options: json, txt, html
+//For this option GENERATE_NEW_REPORT should be true!!!
+const OUTPUT_OPTION = 'html';
 //----------------------------------------------------------
 
+const date = new Date();
+const dirName = `${date.getHours()}H_${date.getMinutes()}M_${date.getDate()}D_${date.getMonth()}M_${date.getFullYear()}Y`.toLowerCase();
+let index = 1;
 
 const readFile = (filePath) => {
     return fs.readFileSync(filePath, 'utf8').split('\r\n');
@@ -29,6 +35,14 @@ const lighthouseCheck = async ({data, newReport = true}) => {
 
     for (const url of data) {
         //check if link is empty
+        if (OUTPUT_OPTION === 'html') {
+            if (!fs.existsSync(`./reports`))
+                fs.mkdirSync(`./reports`);
+            if (!fs.existsSync(`./reports/html`))
+                fs.mkdirSync(`./reports/html`);
+            if (!fs.existsSync(`./reports/html/${dirName}`))
+                fs.mkdirSync(`./reports/html/${dirName}`);
+        }
         if (url.toString().trim().length < 2) continue;
 
         const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
@@ -36,8 +50,14 @@ const lighthouseCheck = async ({data, newReport = true}) => {
         const runnerResult = await lighthouse(url, options);
 
         // `.report` is the HTML report as a string
-        // const reportHtml = runnerResult.report;
-        // fs.writeFileSync('lhreport.html', reportHtml);
+        if (OUTPUT_OPTION === 'html') {
+            // const fileName = url.slice(8).replace('///g', '_').replace('/./g', '_').replace('/-/g', '_') || 'report';
+            const fileName = `report_${index}`
+                || 'report';
+            index++;
+            const reportHtml = runnerResult.report;
+            fs.writeFileSync(`./reports/html/${dirName}/${fileName}.html`, reportHtml);
+        }
 
         const score = runnerResult.lhr.categories.performance.score * 100;
         scoresArray.push({[url]: {score}});
@@ -50,7 +70,6 @@ const lighthouseCheck = async ({data, newReport = true}) => {
     if (!fs.existsSync(`./${OUTPUT_DIRECTORY_NAME}`)) {
         fs.mkdirSync(`./${OUTPUT_DIRECTORY_NAME}`);
     }
-    const date = new Date();
     let formattedDate = `${date.getHours()}:${date.getMinutes()}_${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
     if (!newReport) {
         prevData[formattedDate] = scoresArray;
@@ -62,8 +81,25 @@ const lighthouseCheck = async ({data, newReport = true}) => {
     //generate new report
     formattedDate = `${date.getHours()}H_${date.getMinutes()}M_${date.getDate()}D_${date.getMonth()}M_${date.getFullYear()}Y`.toLowerCase()
     // console.log(formattedDate)
-    jsonfile.writeFileSync(`./${OUTPUT_DIRECTORY_NAME}/report_${formattedDate}.json`, scoresArray, {space: 2/*, flag: 'a'*/});
+    switch (OUTPUT_OPTION) {
+        case "txt": {
+            let formatterResult = '';
+            scoresArray.forEach(el => Object.entries(el).forEach(([key, val]) => formatterResult += `Site: ${key} Score: ${val['score']}\n`))
+            console.log("\n" + formatterResult)
+            fs.writeFileSync(`./${OUTPUT_DIRECTORY_NAME}/report.txt`, formatterResult);
+            break;
+        }
+        case "json": {
+            jsonfile.writeFileSync(`./${OUTPUT_DIRECTORY_NAME}/report_${formattedDate}.json`, scoresArray, {space: 2/*, flag: 'a'*/});
+            break;
+        }
 
+        case "html": {
+            break;
+        }
+
+
+    }
 
 }
 
